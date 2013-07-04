4fa0209a78d0c8a573caf0248165593b0dc43559
BromIDE <herpderpem@yahoo.co.uk>
2013-07-04 16:42:46 +0100

    Updated

--- Misc Python Exercises/py3.py
+++ Misc Python Exercises/py3.py
@@ -1,4 +1,5 @@
 from sys import argv
 script, from_file, to_file = argv
+#See if you can bring the code below down to one line.
 indata = open(from_file,'r').read() #Opens and reads the file
 output = open(to_file,'w').write(indata) #Writes indata to the output file
